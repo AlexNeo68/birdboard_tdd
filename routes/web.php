3245a22ac35599d986ca7c9a23129496a4953db2
alexneo68 <tunning_brain@mail.ru>
2020-06-18 21:38:14 +0300

    first project test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/projects', 'ProjectController@store');
+Route::get('/projects', 'ProjectController@index');
